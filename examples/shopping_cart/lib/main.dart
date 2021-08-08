d1243ad1019cd546959413c2cc3e641008c9da16
Elian Ortega <elian.mortega@gmail.com>
2021-08-07 22:02:41 -0600

    Fix melos test commands

--- examples/shopping_cart/lib/main.dart
+++ examples/shopping_cart/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:shopping_cart/app/app.dart';
 import 'package:search_filter_repository/search_filter_repository.dart';
+
+import 'package:shopping_cart/app/app.dart';
 
 Future<void> main() async {
   const searchFilterRepository = SearchFilterRepository();
