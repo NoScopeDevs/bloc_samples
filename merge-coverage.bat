f577c568459b493b25353b04dcd81f23e255b981
Karlo Verde <karlo.verde.1996@gmail.com>
2021-08-08 04:23:49 -0500

    fix: ensure cov gen consistency

--- merge-coverage.bat
+++ merge-coverage.bat
@@ -1,12 +1,20 @@
 
 @echo off
-set escapePath=%pwd:/=\\%
+setLocal EnableDelayedExpansion
+
+set relPath=!cd:%MELOS_ROOT_PATH%\=!
 
 if exist pubspec.yaml (
   if exist coverage\ (
-    if not exist %MELOS_ROOT_PATH%\coverage_report\ (
-      mkdir %MELOS_ROOT_PATH%\coverage_report
+    if not exist %MELOS_ROOT_PATH%\coverage\ (
+      mkdir %MELOS_ROOT_PATH%\coverage
     )
-    @REM bulk coverage data
+    for /f "delims=" %%a in (coverage\lcov.info) do (
+        SET s=%%a
+        SET s=!s:SF:lib\=SF:%relPath%\lib\!
+        echo !s!>>%MELOS_ROOT_PATH%\coverage\lcov.info
+    )
   )
 )
+
+endLocal
--- merge-coverage.sh
+++ merge-coverage.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-escapedPath="$(echo `pwd` | sed 's/\//\\\//g')"
+relPath="$(echo `pwd` | sed "s/$MELOS_ROOT_PATH\///g" | sed 's/\//\\\//g')"
 
 if [ -f "pubspec.yaml" ]; then
   if [ -d "coverage" ]; then
     if [ ! -d "$MELOS_ROOT_PATH/coverage" ]; then
       mkdir "$MELOS_ROOT_PATH/coverage"
     fi
-    sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> "$MELOS_ROOT_PATH/coverage/lcov.info"
+    sed "s/^SF:lib/SF:$relPath\/lib/g" coverage/lcov.info >> "$MELOS_ROOT_PATH/coverage/lcov.info"
   fi
 fi
