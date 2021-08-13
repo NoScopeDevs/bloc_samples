e41b3ec8766cd32a1c8ce8af181f51c4c97ee9a1
Marcos Sevilla <marcossevilla14@gmail.com>
2021-08-13 14:46:33 -0600

    add HomePage tests

--- examples/preference_navigation/test/home/view/home_page_test.dart
+++ examples/preference_navigation/test/home/view/home_page_test.dart
@@ -1,4 +1,7 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockingjay/mockingjay.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:preference_navigation/home/home.dart';
 import 'package:preference_navigation/preferences/preferences.dart';
@@ -12,6 +15,10 @@ void main() {
   });
 
   group('HomePage', () {
+    test('is routable', () {
+      expect(HomePage.go(), isA<MaterialPageRoute>());
+    });
+
     testWidgets('renders HomePage', (tester) async {
       final preferencesBloc = MockPreferencesBloc();
       when(() => preferencesBloc.state).thenReturn(FakePreferencesState());
@@ -23,5 +30,48 @@ void main() {
       await tester.pump();
       expect(find.byType(HomeView), findsOneWidget);
     });
+  });
+
+  group('HomeView', () {
+    testWidgets(
+      'adds PreferencesCleared when IconButton is pressed',
+      (tester) async {
+        final preferencesBloc = MockPreferencesBloc();
+        when(() => preferencesBloc.state).thenReturn(FakePreferencesState());
+
+        await tester.pumpApp(
+          const HomeView(),
+          preferencesBloc: preferencesBloc,
+        );
+        await tester.pump();
+        await tester.tap(find.byType(IconButton));
+        verify(() => preferencesBloc.add(PreferencesCleared()));
+      },
+    );
+
+    testWidgets(
+      'navigates when PreferencesEmpty is emitted',
+      (tester) async {
+        final navigator = MockNavigator();
+        when(() => navigator.push(any())).thenAnswer((_) async {});
+
+        final preferencesBloc = MockPreferencesBloc();
+        when(() => preferencesBloc.state).thenReturn(FakePreferencesState());
+        whenListen(
+          preferencesBloc,
+          Stream.fromIterable([PreferencesEmpty()]),
+          initialState: PreferencesInitial(),
+        );
+
+        await tester.pumpApp(
+          MockNavigatorProvider(
+            navigator: navigator,
+            child: const HomeView(),
+          ),
+          preferencesBloc: preferencesBloc,
+        );
+        verify(() => navigator.push(any(that: isRoute<void>()))).called(1);
+      },
+    );
   });
 }
