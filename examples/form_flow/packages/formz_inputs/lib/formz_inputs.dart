5e1b62c17d283efa8d1b79e7cd0648f279343387
Marcos Sevilla <marcossevilla14@gmail.com>
2021-08-25 10:23:13 -0600

    add pin form input

--- examples/form_flow/packages/formz_inputs/lib/formz_inputs.dart
+++ examples/form_flow/packages/formz_inputs/lib/formz_inputs.dart
@@ -5,3 +5,4 @@ export 'package:formz/formz.dart';
 export 'src/biography.dart';
 export 'src/email.dart';
 export 'src/name.dart';
+export 'src/pin.dart';
---
+++ examples/form_flow/packages/formz_inputs/test/pin_test.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:formz_inputs/formz_inputs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const pinString = '0123';
+
+  group('PinFormInput', () {
+    group('constructors', () {
+      test('pure creates correct instance', () {
+        final pin = PinFormInput.pure();
+        expect(pin.value, '');
+        expect(pin.pure, true);
+      });
+
+      test('dirty creates correct instance', () {
+        final pin = PinFormInput.dirty(pinString);
+        expect(pin.value, pinString);
+        expect(pin.pure, false);
+      });
+    });
+
+    group('maxLength', () {
+      const newMaxLength = 6;
+
+      test('sets new value correctly', () {
+        PinFormInput.maxLength = newMaxLength;
+        expect(PinFormInput.maxLength, newMaxLength);
+      });
+    });
+
+    group('validator', () {
+      test('returns empty error when pin is empty', () {
+        expect(
+          PinFormInput.dirty().error,
+          PinValidationError.invalid,
+        );
+      });
+
+      test(
+        'returns too long error when pin is longer than default 4 chars',
+        () {
+          expect(
+            PinFormInput.dirty('${pinString}4').error,
+            PinValidationError.invalid,
+          );
+        },
+      );
+
+      test('is valid when pin is valid', () {
+        expect(
+          PinFormInput.dirty(pinString).error,
+          isNull,
+        );
+      });
+    });
+  });
+}
