c31a3f77bdc470c461a806e3310ab1744a9ff637
Marcos Sevilla <marcossevilla14@gmail.com>
2021-08-25 23:14:19 -0600

    add signup events and states

--- examples/form_flow/lib/signup/bloc/sign_up_bloc.dart
+++ examples/form_flow/lib/signup/bloc/sign_up_bloc.dart
@@ -6,5 +6,50 @@ part 'sign_up_event.dart';
 part 'sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc() : super(const SignUpState());
+  SignUpBloc() : super(const SignUpState()) {
+    on<SignUpEmailChanged>(_onEmailChanged);
+    on<SignUpNameChanged>(_onNameChanged);
+    on<SignUpBioChanged>(_onBioChanged);
+    on<SignUpPinChanged>(_onPinChanged);
+  }
+
+  void _onEmailChanged(SignUpEmailChanged event, Emitter emit) {
+    final email = EmailFormInput.dirty(event.email);
+    emit(
+      state.copyWith(
+        email: email,
+        status: Formz.validate([email, state.name, state.biography, state.pin]),
+      ),
+    );
+  }
+
+  void _onNameChanged(SignUpNameChanged event, Emitter emit) {
+    final name = NameFormInput.dirty(event.name);
+    emit(
+      state.copyWith(
+        name: name,
+        status: Formz.validate([state.email, name, state.biography, state.pin]),
+      ),
+    );
+  }
+
+  void _onBioChanged(SignUpBioChanged event, Emitter emit) {
+    final biography = BiographyFormInput.dirty(event.biography);
+    emit(
+      state.copyWith(
+        biography: biography,
+        status: Formz.validate([state.email, state.name, biography, state.pin]),
+      ),
+    );
+  }
+
+  void _onPinChanged(SignUpPinChanged event, Emitter emit) {
+    final pin = PinFormInput.dirty(event.pin);
+    emit(
+      state.copyWith(
+        pin: pin,
+        status: Formz.validate([state.email, state.name, state.biography, pin]),
+      ),
+    );
+  }
 }
--- examples/form_flow/lib/signup/bloc/sign_up_event.dart
+++ examples/form_flow/lib/signup/bloc/sign_up_event.dart
@@ -6,3 +6,39 @@ abstract class SignUpEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class SignUpEmailChanged extends SignUpEvent {
+  const SignUpEmailChanged(this.email);
+
+  final String email;
+
+  @override
+  List<Object> get props => [email];
+}
+
+class SignUpNameChanged extends SignUpEvent {
+  const SignUpNameChanged(this.name);
+
+  final String name;
+
+  @override
+  List<Object> get props => [name];
+}
+
+class SignUpBioChanged extends SignUpEvent {
+  const SignUpBioChanged(this.biography);
+
+  final String biography;
+
+  @override
+  List<Object> get props => [biography];
+}
+
+class SignUpPinChanged extends SignUpEvent {
+  const SignUpPinChanged(this.pin);
+
+  final String pin;
+
+  @override
+  List<Object> get props => [pin];
+}
