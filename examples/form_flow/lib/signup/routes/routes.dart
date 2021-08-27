aec1cffa9821c202dc4d7d293faf296b256a9ce4
Marcos Sevilla <marcossevilla14@gmail.com>
2021-08-27 16:39:09 -0600

    remove unused debugPrint

--- examples/form_flow/lib/signup/routes/routes.dart
+++ examples/form_flow/lib/signup/routes/routes.dart
@@ -4,7 +4,6 @@ import 'package:form_flow/signup/signup.dart';
 
 List<Page> onGenerateSignUpPages(SignUpState state, List<Page> pages) {
   if (state.profile.email.isEmpty && state.profile.name.isEmpty) {
-    debugPrint('to CredentialsPage!');
     return [CredentialsPage.page()];
   } else if (state.profile.biography.isEmpty) {
     return [BiographyPage.page()];
