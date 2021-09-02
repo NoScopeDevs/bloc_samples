e807a224e07f57ae1c81f1940a7e7d2867606f3a
Marcos Sevilla <marcossevilla14@gmail.com>
2021-09-02 10:57:41 -0600

    add onGenerateAppViewPages tests

--- examples/form_flow/lib/app/routes/routes.dart
+++ examples/form_flow/lib/app/routes/routes.dart
@@ -6,8 +6,6 @@ import 'package:form_flow/signup/signup.dart';
 List<Page> onGenerateAppViewPages(AppState state, List<Page> pages) {
   if (state is AppAuthenticated) {
     return [ProfilePage.page(state.user)];
-  } else if (state is AppUnauthenticated) {
-    return [SignUpPage.page()];
   } else {
     return [SignUpPage.page()];
   }
